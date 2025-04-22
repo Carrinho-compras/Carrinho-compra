f6ddbd003bf48d2b4fd43359f875e5ab46ed0b20
AndressaSteffen26 <andressasteffen.ifc.info3@gmail.com>
2025-04-22 11:55:27 -0300

    FEAT: Criação do cabeçalho #4

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,32 @@
 </script>
 
 <template>
+    <body>
+        <header>
+            <div class="logo">
+                <img src="/img/Logo.png" alt="logo">
+                <p> | Apreço <br> a leitura</p>
+            </div>
+            <p>Pesquisar <img src="/img/Search.png" alt="icone-de-lupa"></p>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">Termos</a>
+                    </li>
+                    <li>
+                        <a href="#">Equipe</a>
+                    </li>
+                    <li>
+                        <a href="#">Envio</a>
+                    </li>
+                    <li>
+                        <a href="#">Devoluções</a>
+                    </li>
+                </ul>
+            </nav>
+            
+        </header>
+    </body>
 </template>
 
 <style scoped>

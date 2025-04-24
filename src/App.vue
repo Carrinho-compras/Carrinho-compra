03263322d2b3e3552a7fafc6f033907853bee1fb
AndressaSteffen26 <andressasteffen.ifc.info3@gmail.com>
2025-04-24 11:11:56 -0300

    FEAT: continuação do cabeçalho #4

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@
     <body>
         <header>
             <div class="logo">
-                <img src="/img/Logo.png" alt="logo">
-                <p> | Apreço <br> a leitura</p>
+                <h1>IFBooks</h1>
+                <p class="linha">Apreço <br> a leitura</p>
             </div>
             <p>Pesquisar <img src="/img/Search.png" alt="icone-de-lupa"></p>
             <nav>
@@ -25,10 +25,32 @@
                     </li>
                 </ul>
             </nav>
-            
+            <ul>
+                <li>
+                    <p><img src="/img/Download.png" alt="carrinho"> |</p>
+                </li>
+                <li>
+                    <p><img src="/img/heart active.png" alt="favoritos"> |</p>
+                </li>
+                <li>
+                    <p><img src="/img/User.png" alt="usuario"> |</p>
+                </li>
+            </ul>
         </header>
     </body>
 </template>
 
 <style scoped>
+body header {
+  display: flex;
+  margin: 2vw;
+}
+body header div.logo {
+    display: flex;
+    margin: 0 2vw 0 0;
+}
+body header div.logo p {
+    border-left: 1px solid #27AE60;
+    margin: 0.9vw 2vw 0.9vw 2vw;
+}
 </style>

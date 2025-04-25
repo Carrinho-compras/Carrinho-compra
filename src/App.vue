f74c4c0882f9b77f6d6ff55ffee84c5513310934
AndressaSteffen26 <andressasteffen.ifc.info3@gmail.com>
2025-04-25 15:16:18 -0300

    FEAT: Criação do menu #4

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,18 @@
 <template>
     <body>
         <header>
-            <div class="logo">
+            <div>
+                <div class="logo">
                 <h1>IFBooks</h1>
                 <p class="linha">Apreço <br> a leitura</p>
+                </div>
+                <div class="barra-de-pesquisa">
+                <p class="pesquisar">Pesquisar</p>
+                <img src="/img/Search.png" alt="icone-de-lupa">
+                </div>
             </div>
-            <p>Pesquisar <img src="/img/Search.png" alt="icone-de-lupa"></p>
-            <nav>
+            <div>
+                <nav>
                 <ul>
                     <li>
                         <a href="#">Termos</a>
@@ -27,30 +33,83 @@
             </nav>
             <ul>
                 <li>
-                    <p><img src="/img/Download.png" alt="carrinho"> |</p>
+                    <p><img src="/img/Download.png" alt="carrinho" ></p>
                 </li>
                 <li>
-                    <p><img src="/img/heart active.png" alt="favoritos"> |</p>
+                    <p><i class="fa-solid fa-heart"></i></p>
                 </li>
                 <li>
-                    <p><img src="/img/User.png" alt="usuario"> |</p>
+                    <p><i class="fa-solid fa-user"></i></p>
                 </li>
             </ul>
+            </div>
         </header>
     </body>
 </template>
 
 <style scoped>
-body header {
-  display: flex;
-  margin: 2vw;
+/*------------------------------
+            TOPO
+-------------------------------*/
+header {
+    display: flex;
+    justify-content: space-between;
 }
-body header div.logo {
+header div{
+  display: flex;
+  margin: 2vw 2vw 0 2vw;
+}
+header div.logo {
     display: flex;
     margin: 0 2vw 0 0;
 }
-body header div.logo p {
-    border-left: 1px solid #27AE60;
-    margin: 0.9vw 2vw 0.9vw 2vw;
+header div.logo p.linha {
+    border-left: 2px solid #27AE60;
+    margin: 0.4vw 2vw 4.4vw 1vw;
+    padding: 0 0 0 10px;
+    color: #27AE60;
+    font-size: 1rem;
+}
+header div.barra-de-pesquisa {
+    display: flex;
+    padding: 5px 0vw 5px 5px;
+    background-color: #F1F1F1;
+    margin: 0 0 5vw 2vw;
+}
+header div.barra-de-pesquisa p.pesquisar {
+    font-size: 1.2rem ;
+    padding: 0.5vw 30vw 0 2vw ;
+    color: #B8B8B8;
+}
+header div.barra-de-pesquisa img {
+    width: 80%;
+    height: 100%;
+    padding: 0.9vw 2vw 0.5vw 0;
+    
+}
+header nav ul {
+    list-style: none;
+    display: flex;
+    
+}
+header nav ul li {
+    padding: 1vw 2vw 2vw 0;
+}
+header nav ul li a {
+    text-decoration: none;
+    color: #7B7881;
+}
+header ul {
+    list-style: none;
+    display: flex;
+}
+header ul li {
+    padding: 1vw 2vw 2vw 0vw;
+}
+header ul li i {
+    color: #27AE60;
+    font-size: 1.3rem;
+    border-left: 2px solid #27AE60;
+    padding: 0 0 0 10px;
 }
 </style>

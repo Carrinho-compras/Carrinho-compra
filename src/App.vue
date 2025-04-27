2ce0369910d0de2b05f3437f00b9304a887a38f8
AndressaSteffen26 <andressasteffen.ifc.info3@gmail.com>
2025-04-27 18:55:24 -0300

    FEAT: Conclusão do cabeçalho FEAT: Criação cabeçalho #4

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,11 @@
                 <p class="linha">Apreço <br> a leitura</p>
                 </div>
                 <div class="barra-de-pesquisa">
-                <p class="pesquisar">Pesquisar</p>
+                <p>Pesquisar</p>
                 <img src="/img/Search.png" alt="icone-de-lupa">
                 </div>
             </div>
-            <div>
+            <div class="menu">
                 <nav>
                 <ul>
                     <li>
@@ -29,19 +29,17 @@
                     <li>
                         <a href="#">Devoluções</a>
                     </li>
-                </ul>
-            </nav>
-            <ul>
-                <li>
-                    <p><img src="/img/Download.png" alt="carrinho" ></p>
-                </li>
-                <li>
-                    <p><i class="fa-solid fa-heart"></i></p>
-                </li>
-                <li>
-                    <p><i class="fa-solid fa-user"></i></p>
-                </li>
-            </ul>
+                    <li>
+                        <img src="/img/Download.png" alt="carrinho" >
+                    </li>
+                    <li>
+                        <span class="fa-solid fa-heart"></span>
+                    </li>
+                    <li>
+                        <span class="fa-solid fa-user"></span>
+                    </li>
+                    </ul>
+                </nav>
             </div>
         </header>
     </body>
@@ -51,65 +49,63 @@
 /*------------------------------
             TOPO
 -------------------------------*/
-header {
+
+header{
     display: flex;
     justify-content: space-between;
+    align-items: center;
+    margin: 2vw 0 2vw 0;
+    padding: 0 0 0.5vw 0;
+    border-bottom: 1px solid #27AE60;
 }
 header div{
   display: flex;
-  margin: 2vw 2vw 0 2vw;
+  justify-content: center;
 }
-header div.logo {
+header div.logo{
     display: flex;
-    margin: 0 2vw 0 0;
+    margin: 0 4vw 0 0;
 }
-header div.logo p.linha {
-    border-left: 2px solid #27AE60;
-    margin: 0.4vw 2vw 4.4vw 1vw;
-    padding: 0 0 0 10px;
+header div.logo h1{
+    margin: 1vw 1vw 0 2vw;
+}
+header div.logo p.linha{
+    border-left: 1px solid #27AE60;
+    padding: 0 0 0 1vw;
+    margin: 1vw 0 1.5vw 0;
     color: #27AE60;
-    font-size: 1rem;
+    font-size: 1.05rem;
 }
 header div.barra-de-pesquisa {
     display: flex;
-    padding: 5px 0vw 5px 5px;
-    background-color: #F1F1F1;
-    margin: 0 0 5vw 2vw;
+    margin:  1vw 1vw 1vw 1vw;
+    padding: 10px;
 }
-header div.barra-de-pesquisa p.pesquisar {
-    font-size: 1.2rem ;
-    padding: 0.5vw 30vw 0 2vw ;
+header div.barra-de-pesquisa p {
     color: #B8B8B8;
+    background-color: #F1F1F1;
+    padding:  0.9vw 25vw 0.9vw 1vw;
+    
 }
 header div.barra-de-pesquisa img {
-    width: 80%;
-    height: 100%;
-    padding: 0.9vw 2vw 0.5vw 0;
-    
+    background-color: #F1F1F1;
+    padding:  0.9vw 1vw 0.9vw 0vw;
 }
 header nav ul {
     list-style: none;
     display: flex;
-    
 }
 header nav ul li {
-    padding: 1vw 2vw 2vw 0;
+    margin: 0 1.1vw 0 1.1vw;
 }
 header nav ul li a {
     text-decoration: none;
     color: #7B7881;
 }
-header ul {
-    list-style: none;
-    display: flex;
-}
-header ul li {
-    padding: 1vw 2vw 2vw 0vw;
-}
-header ul li i {
+header nav ul li span {
     color: #27AE60;
     font-size: 1.3rem;
-    border-left: 2px solid #27AE60;
-    padding: 0 0 0 10px;
+    border-left: 1px solid #27AE60;
+    padding: 0 0 0 20px;
 }
 </style>
